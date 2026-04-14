2da902dd1250e66fdd399ab9cb21ffb5f6c1651b
Diegorr <diegoroblesrivera@gmail.com>
2026-04-14 11:44:22 -0400

    primer commit

---
+++ prueba.py
@@ -0,0 +1,15 @@
+# print("Ingrese su nombre")
+# nombre=input()
+# edad=int(input("Ingrese su edad:() ", ))
+
+
+# print("Hola", nombre, "su edad en 5 años será", edad+5)
+
+# Pedir 23 nuemros y mostrar la
+# suma y las resta de ambos
+
+# n1=int(input("ingrese un numero: "))
+# n2=int(input("ingrese otro numero: "))
+
+# print("La suma de los numeros es ", n1+n2)
+# print("La resta de los numeros es ", n1-n2)
