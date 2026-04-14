35b75c7e822819dc7f80ce78a2e80acaf54227db
Diegorr <diegoroblesrivera@gmail.com>
2026-04-14 12:49:24 -0400

    ejemplo de for

---
+++ for.py
@@ -0,0 +1,21 @@
+# Presentacion y ejemplo de for
+# #Ejemplo simple
+# for i in range(3):
+#     print("repeticion N", i+1)
+
+# # Repeticiones dadas por el usuario
+# # Pida al usuariio un numero 
+# # y mustre un saludo esa cant 
+# # de veces.
+
+# num=int(input("Ingrese un num: "))
+# for i in range(num):
+#     print(i+1, "Hola usuario")
+
+# # Pida al usuariio un numero 
+# # muestre la tabla de multiplicar
+# # de ese numero hasta el 10.
+
+# num=int(input("Ingrese un num: "))
+# for i in range(10):
+#     print(num ,"x",i+1, "=", num*(i+1))
--- prueba.py
+++ prueba.py
@@ -13,3 +13,21 @@
 
 # print("La suma de los numeros es ", n1+n2)
 # print("La resta de los numeros es ", n1-n2)
+
+# calcular el iva para cada compra
+print(
+'''
+1.- Pera 1200
+2.- Manzana 1400
+3.- Piña 2000
+''')
+select=int(input("Seleccione una fruta: "))
+if select==1:
+    print("El total a pagar es", 1200*1.19)
+elif select==2:
+    print("El total a pagar es", 1400*1.19)
+elif select==3:
+    print("El total a pagar es", 2000*1.19)
+else:
+    print("Numero no valido")
+
